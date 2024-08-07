d33cb829fa68667c19e0a35de7f60ac700168e42
vovacgscaut <167454133+vovacgscaut@users.noreply.github.com>
2024-08-07 09:46:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+
+date=input("Введите дату")
+point=input("Введите задачу")
+date1=input("Введите дату")
+point1=input("Введите задачу")
+date2=input("Введите дату")
+point2=input("Введите задачу")
+notepad={"day": [],
+         "day1":[],
+         "day2":[]}
+day_key=notepad["day"]
+day_key.append("point")
+day_key1=notepad["day1"]
+day_key1.append("point1")
+day_key2=notepad["day2"]
+day_key2.append("point2")
+print(notepad)
+#print(date,point,date1,point1,date2,point2) вывод по отдельности
